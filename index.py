af7517a151b356e0ff84eb621d0462dae3bf402d
root <root@mx>
2022-10-18 21:39:56 +0100

    playing with selenium

--- index.py
+++ index.py
@@ -4,3 +4,5 @@ PATH = '/home/fedegot/Documents/chromedriver'
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://consmanchester.esteri.it")
+print(driver.title) ## return the title
+#driver.quit()
