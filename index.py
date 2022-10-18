6d1c2d9f54d1a583bbff01764345c0aa901c62be
fedegot <gotfede@gmail.comn>
2022-10-18 22:43:07 +0100

    login

--- index.py
+++ index.py
@@ -1,8 +1,30 @@
-from selenium import webdriver 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from pyinputplus import inputEmail, inputPassword
+import time
 
+
+email = inputEmail("Inserisci la tua email: ")
+paswd = inputPassword("Inserisci la tua password: ")
 PATH = '/home/fedegot/Documents/chromedriver'
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://consmanchester.esteri.it")
+driver.get("https://prenotami.esteri.it/")
 print(driver.title) ## return the title
-#driver.quit()
+
+log_usr = driver.find_element(By.ID,"login-email")
+log_usr.send_keys(email)
+pswd_usr = driver.find_element(By.ID,"login-password")
+pswd_usr.send_keys(paswd)
+
+pswd_usr.send_keys(Keys.RETURN)
+
+# rkPEFKH52h%p!5
+prn = driver.find_element(By.ID, "advanced").send_keys(Keys.RETURN)
+#prenot = driver.find_element(By.XPATH, "https://prenotami.esteri.it/Services/Booking/1137").send_keys(Keys.RETURN)
+driver.navigate().to('https://prenotami.esteri.it/Services/Booking/1137')
+
+time.sleep(5)
+
+driver.quit()
