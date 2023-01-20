652d6808c3cd8cef4996e24122afc36e849e5a6c
fedegot <gotfede@gmail.com>
2023-01-20 10:45:34 +0000

    aspettare bottone

--- index.py
+++ index.py
@@ -5,13 +5,15 @@ from pyinputplus import inputEmail, inputPassword
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 from termcolor import colored, cprint
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 email = inputEmail("Inserisci la tua email: ")
 paswd = inputPassword("Inserisci la tua password: ")
 path2  = input(cprint((f"Premi:\n 1. Per Passaporto \n 2. Per Stato Civile \n 3. Carta D'Identita' \n"), 'red', attrs=['bold']))
 PATH  = 'C:/Users/got_a/OneDrive/Documents/msedgedriver'
 driver = webdriver.Edge(PATH)
+print(webdriver.__version__)
 
 driver.get("https://prenotami.esteri.it/")
 print(driver.title) ## return the title
@@ -28,8 +30,13 @@ print(f"You're inside your personal page{email}")
 
 # rkPEFKH52h%p!5
 # //button[@class='button primary']
-pren = driver.find_element(By.XPATH, f"//button[@class='button primary'][{path2}]")
-driver.implicitly_wait(1000)
+
+
+
+driver.implicitly_wait(8000)
+pren = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, f"//button[@class='button primary'][{path2}]"))
+)
 ActionChains(driver).move_to_element(pren).click(pren).perform()
 driver.implicitly_wait(10)
 #if driver.find_element(By.CLASS_NAME, "jc-bs3-container container") == True:
