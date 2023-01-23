616d7f1ebaf38dde1822f4a7128aa29d35553cda
fedegot <gotfede@gmail.com>
2023-01-23 07:50:07 +0000

    asdas

--- index.py
+++ index.py
@@ -15,6 +15,14 @@ from selenium.webdriver.support.ui import Select
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('start-maximized')
+options.add_argument('disable-infobars')
+options.add_argument('--disable-gpu')
+options.add_argument('--remote-debugging-port=9222')
+
+driver = webdriver.Chrome(chrome_options=options)
 
 
 email = inputEmail("Inserisci la tua email: ")
